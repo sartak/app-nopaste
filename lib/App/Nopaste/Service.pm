6899edbc2ab70c8c66a04f37461519711c0a6779
Shawn M Moore <sartak@gmail.com>
2008-03-09 09:39:05 -0400

    Document the methods of App::Nopaste::Service

--- lib/App/Nopaste/Service.pm
+++ lib/App/Nopaste/Service.pm
@@ -70,6 +70,65 @@ App::Nopaste::Service - base class for nopaste services
 
 =head1 DESCRIPTION
 
+C<App::Nopaste::Service> defines a generic interface for uploading to nopaste
+sites. It provides a default interface to that of the POE Pastebot.
+
+=head1 METHODS
+
+=head2 nopaste
+
+This is the outermost method called by L<App::Nopaste> and other clients. You
+should not override this method, as it will (XXX: eventually) perform
+canonicalization of arguments (such as C<lang>) for you.
+
+=head2 run args -> (OK, message)
+
+This is the outermost method you should override. You'll be passed a hash of arguments. The only arguments you should pay attention to are:
+
+=over 4
+
+=item text
+
+The body of text to paste.
+
+=item desc
+
+A short (one-line) summary of the paste.
+
+=item nick
+
+The name of the person performing the paste.
+
+=item chan
+
+The IRC channel to which the paste belongs.
+
+=item lang
+
+The programming language of the body of text.
+
+=back
+
+=head2 get mech, args
+
+This should "get" the form to paste using the provided L<WWW::Mechanize>
+object. By default it does just that. See L</uri> below.
+
+=head2 uri
+
+If you only need to call C<< mech->get(uri) >> then you may define this method
+to provide the URI of the nopaste service.
+
+=head2 fill_form mech, args
+
+This should have the L<WWW::Mechanize> fill in the form using the arguments,
+and submit it.
+
+=head2 return mech, args
+
+This should look at C<< WWW::Mechanize->content >> to find the URI to the
+pasted text.
+
 =head1 SEE ALSO
 
 L<App::Nopaste>
