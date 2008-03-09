aac0e805edb72e3a7837bfa6f7481aa82e172506
Shawn M Moore <sartak@gmail.com>
2008-03-09 09:42:04 -0400

    Add Snitch - http://nopaste.snit.ch/

---
+++ lib/App/Nopaste/Service/Snitch.pm
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+package App::Nopaste::Service::Snitch;
+use strict;
+use warnings;
+use base 'App::Nopaste::Service';
+
+sub uri { "http://nopaste.snit.ch" }
+
+=head1 NAME
+
+App::Nopaste::Service::Snitch - http://nopaste.snit.ch/
+
+=cut
+
+1;
+
