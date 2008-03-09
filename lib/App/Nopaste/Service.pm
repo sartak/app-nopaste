8438297c95a70ca504f2114e145fdacd069af71f
Shawn M Moore <sartak@gmail.com>
2008-03-09 10:01:23 -0400

    Cut down some POD in App::Nopaste::Service

--- lib/App/Nopaste/Service.pm
+++ lib/App/Nopaste/Service.pm
@@ -129,28 +129,9 @@ and submit it.
 This should look at C<< WWW::Mechanize->content >> to find the URI to the
 pasted text.
 
-=head1 SEE ALSO
-
-L<App::Nopaste>
-
 =head1 AUTHOR
 
 Shawn M Moore, C<< <sartak at gmail.com> >>
-
-=head1 BUGS
-
-No known bugs.
-
-Please report any bugs through RT: email
-C<bug-app-nopaste at rt.cpan.org>, or browse
-L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Nopaste>.
-
-=head1 COPYRIGHT AND LICENSE
-
-Copyright 2008 Shawn M Moore.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
 
 =cut
 
