3092c60d82892bf7e270f2339c0812a923fd541a
Shawn M Moore <sartak@gmail.com>
2008-03-09 07:48:52 -0400

    Initial import of App::Nopaste

---
+++ lib/App/Nopaste.pm
@@ -0,0 +1,83 @@
+#!perl
+package App::Nopaste;
+use strict;
+use warnings;
+
+
+
+=head1 NAME
+
+App::Nopaste - ???
+
+=head1 VERSION
+
+Version 0.01 released ???
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+    use App::Nopaste;
+    do_stuff();
+
+=head1 DESCRIPTION
+
+
+
+=head1 SEE ALSO
+
+L<Foo::Bar>
+
+=head1 AUTHOR
+
+Shawn M Moore, C<< <sartak at gmail.com> >>
+
+=head1 BUGS
+
+No known bugs.
+
+Please report any bugs through RT: email
+C<bug-app-nopaste at rt.cpan.org>, or browse
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Nopaste>.
+
+=head1 SUPPORT
+
+You can find this documentation for this module with the perldoc command.
+
+    perldoc App::Nopaste
+
+You can also look for information at:
+
+=over 4
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/App-Nopaste>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/App-Nopaste>
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=App-Nopaste>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/App-Nopaste>
+
+=back
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright 2007 Shawn M Moore.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+1;
+
---
+++ t/001-basic.t
@@ -0,0 +1,8 @@
+#!perl -T
+use strict;
+use warnings;
+use Test::More tests => 1;
+use App::Nopaste;
+
+ok(1);
+
