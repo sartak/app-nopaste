726e16b34b04cb82e21551e03e77568d6fda05c4
Shawn M Moore <sartak@gmail.com>
2008-12-03 21:52:45 -0500

    Bump to 0.07

--- lib/App/Nopaste.pm
+++ lib/App/Nopaste.pm
@@ -7,7 +7,7 @@ use Module::Pluggable search_path => 'App::Nopaste::Service';
 use base 'Exporter';
 our @EXPORT_OK = 'nopaste';
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 sub nopaste {
     # process arguments {{{
