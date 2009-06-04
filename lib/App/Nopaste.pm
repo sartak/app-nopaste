b6bf38be80b03b96f800e8f9fbadf104c96c58e6
Shawn M Moore <sartak@gmail.com>
2009-06-04 14:55:42 -0400

    Update copyright

--- lib/App/Nopaste.pm
+++ lib/App/Nopaste.pm
@@ -169,7 +169,7 @@ L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=App-Nopaste>.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008 Shawn M Moore.
+Copyright 2008-2009 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
