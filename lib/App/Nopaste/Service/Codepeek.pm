9a6a45c684f930378c6309d1b6718f8d44c0b74e
Justin Hunter <justin.d.hunter@gmail.com>
2010-04-17 02:03:52 +0800

    add Codepeek service

---
+++ lib/App/Nopaste/Service/Codepeek.pm
@@ -0,0 +1,40 @@
+package App::Nopaste::Service::Codepeek;
+use strict;
+use warnings;
+use base 'App::Nopaste::Service';
+
+sub uri { 'http://codepeek.com/paste' }
+
+sub fill_form {
+    my $self = shift;
+    my $mech = shift;
+    my %args = @_;
+
+    $mech->form_number(1);
+
+    $mech->submit_form(
+        fields => {
+            'paste'    => $args{text},
+            'language' => ucfirst $args{lang},
+        },
+    );
+}
+
+sub return {
+    my $self = shift;
+    my $mech = shift;
+
+    my $link = $mech->find_link( text_regex => qr#^http://codepeek.com/paste/[a-z0-9]+$#i );
+    my $url = $link->url;
+    return (0, "Could not construct paste link.") if !$url;
+    return (1, $url);
+}
+
+1;
+
+=head1 NAME
+
+App::Nopaste::Service::Codepeek - http://codepeek.com
+
+=cut
+
