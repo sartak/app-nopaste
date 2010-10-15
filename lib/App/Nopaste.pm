14e7aa8786ab671d996d1497690d39bb86625176
Justin Hunter <justin.d.hunter@gmail.com>
2010-10-15 14:47:46 -0700

    change copyright year

--- lib/App/Nopaste.pm
+++ lib/App/Nopaste.pm
@@ -164,7 +164,7 @@ Shawn M Moore, C<sartak@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2008-2009 Shawn M Moore.
+Copyright 2008-2010 Shawn M Moore.
 
 This program is free software; you can redistribute it and/or modify it
 under the same terms as Perl itself.
