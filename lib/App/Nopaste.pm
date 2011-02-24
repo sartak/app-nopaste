348a826f33cfb2bc7ec96de7f048567533e817de
Shawn M Moore <sartak@bestpractical.com>
2011-02-23 23:46:09 -0500

    0.26 and its change

--- lib/App/Nopaste.pm
+++ lib/App/Nopaste.pm
@@ -7,7 +7,7 @@ use Module::Pluggable search_path => 'App::Nopaste::Service';
 use base 'Exporter';
 our @EXPORT_OK = 'nopaste';
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 sub nopaste {
     # process arguments {{{
